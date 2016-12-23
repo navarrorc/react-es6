387363be39d3982053656d079cb37064fc563e5d
Roberto <roberto@austincodingacademy.com>
2016-12-23 19:46:15 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# react-es6
+
+> A React.js project
+
+## Build Setup
+
+``` bash
+# install (Run)
+./setup.sh
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the ...[TODO: add Url here]
---
+++ setup.sh
@@ -0,0 +1,114 @@
+#npm init --yes
+
+cat > package.json <<DELIM
+{
+  "name": "react-es6",
+  "description": "A React.js project",
+  "version": "1.0.0",
+  "author": "Roberto C. Navarro",
+  "private": true,
+  "scripts": {
+    "dev": "cross-env NODE_ENV=development webpack-dev-server --host='0.0.0.0' --open --inline --hot",
+    "build": "cross-env NODE_ENV=production webpack --progress --hide-modules"
+  },
+  "dependencies": {
+  },
+  "devDependencies": {
+  }
+}
+DELIM
+
+npm install --save react react-dom
+npm install --save-dev webpack@beta webpack-dev-server@beta babel-loader babel-preset-es2015 babel-core babel-preset-react cross-env
+
+# Configure Babel
+echo '{ "presets": ["react","es2015"] }' > .babelrc
+
+cat > webpack.config.js <<DELIM
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/main.jsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#inline-source-map' //see: bit.ly/2i0EF8h '#eval-source-map'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
+}
+DELIM
+
+cat > index.html <<DELIM
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="UTF-8">
+      <title>Title</title>
+  </head>
+  <body>
+    <div id="root"></div>
+     
+    <script type="text/javascript" src="/dist/build.js"></script>
+     
+  </body>
+</html>
+DELIM
+
+mkdir app
+cat > app/main.jsx <<DELIM
+import React ,{Component} from 'react';
+import ReactDOM from 'react-dom';
+ 
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+ 
+ReactDOM.render(<App />,document.getElementById('root'));
+DELIM
+
+
