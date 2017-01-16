09c0ce58a2d1389406d4824a1d9c7edf85bcd626
Roberto C Navarro <navarrorc@users.noreply.github.com>
2017-01-16 13:27:57 -0600

    added json-loader module section

--- setup.sh
+++ setup.sh
@@ -36,14 +36,21 @@ module.exports = {
     filename: 'build.js'
   },
   module: {
-    loaders: [{
-      test: /\.jsx$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
-      }
-    }]
+    loaders: [
+        {
+          test: /\.jsx$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.json$/,
+          exclude: /(node_modules)/,
+          loader: 'json-loader'
+        }
+    ]
   },
   devServer: {
     historyApiFallback: true,
